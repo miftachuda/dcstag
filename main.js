bcab5c7028339d1281c09094dc7f1831c711ef67
Miftachul Huda <miftachulcc@gmail.com>
2022-09-25 13:20:01 +0700

    change to fusejs

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-
 import './style.css';
 import './input.css';
 
 import data from './data.json';
-import exe from 'itemsjs';
+import Fuse from 'fuse.js';
 
-var itemsjs = exe(data, {
-  sortings: {
-    name_asc: {
-      field: 'tagname',
-      order: 'asc'
-    }
-  },
-  searchableFields: ['tagname', 'desc']
-});
 
-function searchTag(keyword) {
-  return itemsjs.search({
-    per_page: 35,
-    sort: 'name_asc',
-    query: keyword,
-
-  })
-}
 
 var input = document.getElementById('keyword')
 var output = document.getElementById('app')
+
 input.addEventListener('input', (x) => {
+
   if (input.value != "") {
+    var keys;
+    if (isNaN(input.value)) {
+      keys = ['tagname', 'desc']
+    } else {
+      keys = ['tagname']
+    }
+
+    const fuse = new Fuse(data, {
+      shouldSort: true,
+      includeScore: true,
+      threshold: 0.3,
+      ignoreFieldNorm: true,
+      keys: keys
+    })
+
+    var result = fuse.search(input.value)
+    if (result.length > 50) {
+      result = result.splice(0, 50)
+    }
     const regex = new RegExp(input.value, 'gi');
-    var result = searchTag(input.value)
-    //console.log(result)
-    var resulthtml = result.data.items.map((x) => {
-      //console.log(x)
-      return `<div class="card"><div class="tag">${x.tagname}</div><div class="desc">${x.desc}</div></div>`
+    var resulthtml = result.map((x) => {
+      return `<div class="card"><div class="tag">${x.item.tagname}</div><div class="desc">${x.item.desc}</div></div>`
     })
     output.innerHTML = resulthtml.join("")
 
